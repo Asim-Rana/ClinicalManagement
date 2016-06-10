3f8bb128476f6cb061a8320af7496a229994a87d
Asim-Rana <asim.irshad16@gmail.com>
2016-06-10 12:05:49 +0500

    addition of clinical management

---
+++ ClinicalManagement/ClinicalManagement/app/scripts/app.js
@@ -0,0 +1,45 @@
+﻿var mainApp = angular.module('mainApp', ['ngRoute', 'clinicControllers']);
+
+mainApp.config([
+    '$routeProvider',
+    function ($routeProvider) {
+        $routeProvider.
+            when('/', {
+                templateUrl : 'views/home.html',
+                controller  : 'mainController'
+            })
+
+            .when('/about', {
+                templateUrl: 'views/about.html',
+                controller  : 'aboutController'
+            })
+            .when('/services', {
+                templateUrl: 'views/services.html',
+                controller: 'serviceController'
+            })
+
+            .when('/appointment', {
+                templateUrl: 'views/appointment.html',
+                controller: 'appointmentController'
+            })
+
+            .when('/contact', {
+                templateUrl: 'views/contact.html',
+                controller: 'contactController'
+            })
+
+            .when('/login', {
+                templateUrl: 'views/login.html',
+                controller: 'loginController'
+            })
+
+            .when('/signup', {
+                templateUrl: 'views/signup.html',
+                controller: 'signupController'
+            })
+
+            .otherwise({
+                redirectTo: '/'
+            });
+        }
+]);
